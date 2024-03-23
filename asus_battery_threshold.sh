0d9f908f3784dd5b259f4aa234d816a55b245f37
hoangtv49 <codyhoang.it@gmail.com>
2024-03-23 10:13:17 +0700

    fix: add setting to crontab every reboot time

--- asus_battery_threshold.sh
+++ asus_battery_threshold.sh
@@ -1,1 +1,2 @@
-echo 81 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold
+sudo nano /etc/crontab
+@reboot root echo 80 > /sys/class/power_supply/BAT0/charge_control_end_threshold
