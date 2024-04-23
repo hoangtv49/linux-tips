b5b3bc53c947b7f095702c697e64daf0a9fd20db
hoangtv49 <92872128+hoangtv49@users.noreply.github.com>
2024-04-23 10:08:14 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,4 @@
-udo apt-get update
+sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings and curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg]
